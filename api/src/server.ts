1f9a2952596608ae2ee47ad79acf2a402c26e10c
MJ Adendorff <mj.praetor@gmail.com>
2022-09-30 16:57:05 +0200

    removed non-stream & added check for file errors

--- api/src/server.ts
+++ api/src/server.ts
@@ -67,12 +67,12 @@ app.post("/stream", (req, res) => {
 	if (req.body.path) {
 		DIR = req.body.path;
 	} else {
-		DIR = "/";
+		DIR = ".";
 	}
 	// Read directory
 	fs.readdir(DIR, (error, files) => {
 		if (error) {
-			res.status(500).json({ error: "could not read directory/file" });
+			res.status(500).json({ error: "could not read directory" });
 			return;
 		}
 		// structure data as JSON array
@@ -85,63 +85,26 @@ app.post("/stream", (req, res) => {
 			}
 			count++;
 			let filePath = `${DIR}/${file}`;
-			let stats = fs.statSync(filePath);
-			let newFile: File = {
-				fileName: file,
-				fullPath: filePath,
-				dateCreated: stats.birthtime,
-				permissions: getPermissions(stats.mode),
-				isDirectory: stats.isDirectory(),
-				fileType: getExtension(file),
-				size: getKilobytesFromBytes(stats.size),
-			};
-			// append file info to listing
-			res.status(200).write(JSON.stringify(newFile));
+			try {
+				let stats = fs.statSync(filePath);
+				let newFile: File = {
+					fileName: file,
+					fullPath: filePath,
+					dateCreated: stats.birthtime,
+					permissions: getPermissions(stats.mode),
+					isDirectory: stats.isDirectory(),
+					fileType: getExtension(file),
+					size: getKilobytesFromBytes(stats.size),
+				};
+				// append file info to listing
+				res.status(200).write(JSON.stringify(newFile));
+			} catch {
+				console.warn(`error reading file ${file}, ignoring...`);
+			}
 		});
 		// close the stream and JSON data structure
 		res.write("]");
 		res.status(200).end();
-	});
-});
-
-/**
- * * POST request
- * This endpoint doesn't stream data
- * Queries the path found in the body (if it exists) on the local filesystem
- * and returns the directory listing if it's a directory.
- */
-app.post("/listing", (req, res) => {
-	// Get DIRECTORY PATH from POST body
-	let DIR: string;
-	if (req.body.path) {
-		DIR = req.body.path;
-	} else {
-		DIR = "/";
-	}
-	let listing: Listing = [];
-	// Read directory contents
-	fs.readdir(DIR, (error, files) => {
-		if (error) {
-			res.status(500).json({ error: "could not read directory/file" });
-			return;
-		}
-		// Loop through items in directory
-		files.forEach((file) => {
-			let filePath = `${DIR}/${file}`;
-			let stats = fs.statSync(filePath);
-			let newFile: File = {
-				fileName: file,
-				fullPath: filePath,
-				dateCreated: stats.birthtime,
-				permissions: getPermissions(stats.mode),
-				isDirectory: stats.isDirectory(),
-				fileType: getExtension(file),
-				size: getKilobytesFromBytes(stats.size),
-			};
-			// append file info to listing
-			listing.push(newFile);
-		});
-		res.status(200).json(listing);
 	});
 });
 
